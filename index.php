0230c792af6e44d4218fe2b88ed2364af434d0cf
Ibrokhim Abdukaimov <ibrokhim19990505@gmail.com>
2021-02-25 14:55:00 +0500

    new fetch method added

--- index.php
+++ index.php
@@ -4521,8 +4521,8 @@
                                                 ValidUkPassport:'',
                                                 Addresses:'' 
                                 }
-                                var employment=''            
-                                        document.querySelectorAll('input').forEach((item) => 
+                                var employment='';            
+                                document.querySelectorAll('input').forEach((item) => 
                                         {
                                            if(item.name==='VehicleType'||item.id==='VehicleType'){
                                                data.VehicleType=item.value
@@ -4591,34 +4591,53 @@
                                            }
                                            
                                         });
-                                        data.Employments.push(employment)
-                                        data.Applicants.push(singleApplicantData)
-                                        console.log('collected form data=> ',data)
-                                        async function postData(url = '', postData = {}) {
-                                             // Default options are marked with *
-                                             const response = await fetch(url, {
-                                             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                                             mode: 'no-cors',// no-cors, *cors, same-origin
-                                             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                                             credentials: 'same-origin', // include, *same-origin, omit
-                                             headers: {
-                                            'Access-Control-Allow-Origin': '*',
-                                            'Content-Type': 'application/json', // 'Content-Type': 'application/x-www-form-urlencoded',
-                                            'X-ApiKey': '19ff541eb45e4ac58cdadc457868211b',
-                                             },
-                                             redirect: 'follow', // manual, *follow, error
-                                             referrerPolicy: 'no-referrer', // no-referrer, *client
-                                             body: JSON.stringify(postData) // body data type must match "Content-Type" header
-                                             });
-                                             return await response // parses JSON response into native JavaScript objects
-                                            }
+                                data.Employments.push(employment)
+                                data.Applicants.push(singleApplicantData)
+                                console.log('collected form data=> ',data)
+                                // async function postData(url = '', postData = {}) {
+                                //              // Default options are marked with *
+                                //              const response = await fetch(url, {
+                                //              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                                //              mode: 'no-cors',// no-cors, *cors, same-origin
+                                //              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                                //              credentials: 'same-origin', // include, *same-origin, omit
+                                //              headers: {
+                                //             'Access-Control-Allow-Origin': '*',
+                                //             'Content-Type': 'application/json', // 'Content-Type': 'application/x-www-form-urlencoded',
+                                //             'X-ApiKey': '19ff541eb45e4ac58cdadc457868211b',
+                                //              },
+                                //              redirect: 'follow', // manual, *follow, error
+                                //              referrerPolicy: 'no-referrer', // no-referrer, *client
+                                //              body: JSON.stringify(postData) // body data type must match "Content-Type" header
+                                //              });
+                                //              return await response // parses JSON response into native JavaScript objects
+                                // }
 
-                                        postData("https://api.autoconvert.co.uk/application/submit?=", data)
-                                        .then((response) => {
-                                        console.log(response); // JSON data parsed by `response.json()` call
-                                        });
-                                        
-                                    });
+                                // postData("https://api.autoconvert.co.uk/application/submit?=", data)
+                                //         .then((response) => {
+                                //         console.log(response); // JSON data parsed by `response.json()` call
+                                // });
+                                
+                                var myHeaders = new Headers();
+                                myHeaders.append("Content-Type", "application/json");
+                                myHeaders.append("X-ApiKey", "19ff541e-b45e-4ac5-8cda-dc457868211b");
+                                myHeaders.append("mode", "cors");
+                                
+                                var raw=JSON.stringify(data)
+
+                                var requestOptions = {
+                                method: 'POST',
+                                headers: myHeaders,
+                                body: raw,
+                                redirect: 'follow'
+                                };  
+
+                                fetch("https://api.autoconvert.co.uk/application/submit?=", requestOptions)
+                                .then(response => response.text())
+                                .then(result => console.log(result))
+                                .catch(error => console.log('error', error));
+
+                            });
                             }
                             
                             </script>
